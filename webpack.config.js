2e7c254356cee7ea0b6db8c199ab0e0c7402f34f
Victor Storozhenko <storozvic@gmail.com>
2020-03-07 13:53:17 -0800

    webpack babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,21 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      }
+    ]
   }
 }
