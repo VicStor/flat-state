396c64b9b16abe2ffe756ed3c69f96a487e7877f
Victor Storozhenko <storozvic@gmail.com>
2020-03-07 14:05:15 -0800

    Rename, babel, source maps (#3)
    * rename to flat-state
    
    * webpack babel-loader
    
    * webpack babel, source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path')
 
+const mode = process.env.NODE_ENV || 'development'
+
 module.exports = {
-  mode: 'production',
+  mode,
+  target: 'node',
+  stats: 'verbose',
   entry: {
     index: './src/index.js',
     store: './src/store.js',
@@ -10,5 +14,23 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    modules: ['node_modules', path.resolve(__dirname, 'src')]
   }
 }
